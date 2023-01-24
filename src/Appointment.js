e367ecd597d3334e79bf007ee17ed5862699ed84
Lachlan MacKenzie <lachlanrm@gmail.com>
2023-01-24 13:03:36 -0800

    Passing first test

---
+++ src/Appointment.js
@@ -0,0 +1,5 @@
+// export const Appointment = ({ appointment }) => (
+//   <div>{appointment.customer.firstName}</div>
+// );
+
+export const Appointment = () => "Ashley";
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Appointment } from "../src/Appointment";
+import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
   it("renders the customer first name", () => {
@@ -7,7 +9,8 @@ describe("Appointment", () => {
     const component = <Appointment customer={customer} />;
     const container = document.createElement("div");
     document.body.appendChild(container);
-    ReactDOM.createRoot(container).render(component);
+    act(() => ReactDOM.createRoot(container).render(component));
+
     expect(document.body.textContent).toContain("Ashley");
   });
 });
