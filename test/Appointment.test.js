704c539f35c215a4df0b6720b9d78c2ff9634c1e
Lachlan MacKenzie <lachlanrm@gmail.com>
2023-01-25 17:38:29 -0800

    Test refactor -- BeforeEach

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,19 +4,23 @@ import { Appointment } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
+
     act(() => ReactDOM.createRoot(container).render(component));
     expect(document.body.textContent).toContain("Ashley");
   });
   it("renders the customer first name", () => {
     const customer = { firstName: "Jimbo" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
+
     act(() => ReactDOM.createRoot(container).render(component));
     expect(document.body.textContent).toContain("Jimbo");
   });
