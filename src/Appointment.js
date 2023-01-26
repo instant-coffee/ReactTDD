1b7120a076524a75ed8edf63e586cee919c839f0
Lachlan MacKenzie <lachlanrm@gmail.com>
2023-01-26 10:46:19 -0800

    Render Day view

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,2 +1,6 @@
 import React from "react";
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+
+export const AppointmentDayView = ({ appointments }) => (
+  <div id="appointmentsDayView" />
+);
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentDayView } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
@@ -23,5 +23,23 @@ describe("Appointment", () => {
 
     act(() => ReactDOM.createRoot(container).render(component));
     expect(document.body.textContent).toContain("Jimbo");
+  });
+});
+
+describe("AppointmentDayView", () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) => {
+    act(() => ReactDOM.createRoot(container).render(component));
+  };
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentDayView appointments={[]} />);
+
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
 });
