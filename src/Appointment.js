8954fbbc42b040671d90e9cc0822087fe8d6f087
Lachlan MacKenzie <lachlanrm@gmail.com>
2023-01-27 21:31:34 -0800

    WIP SEND HELP

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,6 +3,10 @@ export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
 export const AppointmentDayView = ({ appointments }) => (
   <div id="appointmentsDayView">
-    <ul></ul>
+    <ul>{appointments.map(appointment) => {
+      return(
+        <li key={appointment.startsAt}>{appointment.customer.firstName}</li>
+      )
+    }}</ul>
   </div>
 );
